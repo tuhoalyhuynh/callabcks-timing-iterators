0f2a97ae942aec3b5b134ab5d74a01ebe37c25f0
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-10-22 10:18:38 -0400

    [js] Add check number function

---
+++ checknumber.js
@@ -0,0 +1,35 @@
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(-5));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(0));
