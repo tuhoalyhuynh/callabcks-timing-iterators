37b0652944f887e34b4d511b660aa253b83e6784
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-10-22 10:14:54 -0400

    [js] Add last 5 hello functions

--- hello.js
+++ hello.js
@@ -20,15 +20,50 @@ function sayHello(name) {
 console.log(sayHello('Pete'));
 
 function sayHello(name) {
-    let msg = 'Hello, ' + name + '! How are you?'
+    let msg = 'Hello, ' + name + '! How are you?';
     return msg;
 }
 
 console.log(sayHello('Dexter'));
 
 function sayHello(name) {
-    let msg = 'Hello, ' + name + '! How are you?'
+    let msg = 'Hello, ' + name + '! How are you?';
     return msg;
 }
 
 console.log(sayHello('Mike'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '! How are you?';
+    return msg;
+}
+
+console.log(sayHello('Luke'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '! How are you?';
+    return msg;
+}
+
+console.log(sayHello('Jaxon'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '! How are you?';
+    return msg;
+}
+
+console.log(sayHello('Nicole'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '! How are you?';
+    return msg;
+}
+
+console.log(sayHello('Jackson'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '! How are you?';
+    return msg;
+}
+
+console.log(sayHello('Tu Anh'))
